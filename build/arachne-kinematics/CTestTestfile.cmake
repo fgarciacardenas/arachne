4260ba0bf34e15b15d8c0e12ea6e59e909c85861
fgarciacardenas <facundoxgc@gmail.com>
2020-08-22 15:03:50 -0500

    Cleaning repository

--- build/arachne-kinematics/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/facundo/arachne/src/arachne-kinematics
-# Build directory: /home/facundo/arachne/build/arachne-kinematics
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/arachne-simulation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/facundo/arachne/src/arachne-simulation
-# Build directory: /home/facundo/arachne/build/arachne-simulation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
